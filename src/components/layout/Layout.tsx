2e60f954b3df1e0e09ac06db52fc34991297f93d
KZHiganov <kzhiganov@zvezdamedia.ru>
2024-08-29 10:52:40 +0300

    Сверстал хеддер

--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -1,9 +1,17 @@
-import React, { FC } from 'react';
+import React, {FC} from 'react';
+import {Header} from "./header/Header";
+import {Footer} from "./footer/Footer";
+import {Banner} from "../banner/Banner";
 
 interface IProps {
-  children: React.ReactElement;
+    children: React.ReactElement;
 }
 
-export const Layout: FC<IProps> = ({ children }) => {
-  return <div>{children}</div>;
+export const Layout: FC<IProps> = ({children}) => {
+    return <div>
+        <Header/>
+        <Banner/>
+        {children}
+        <Footer/>
+    </div>;
 };
--- src/components/layout/header/Header.tsx
+++ src/components/layout/header/Header.tsx
@@ -1,5 +1,23 @@
 import React from 'react';
+import {Link} from "react-router-dom";
+import styles from './Header.module.scss'
+
+import logo from 'assets/img/logo.png'
+import clsx from "clsx";
 
 export const Header = () => {
-  return <div></div>;
+    return <header>
+        <div className={styles.headerRow}>
+            <Link to="/" className={styles.logo}>
+                <img src={logo} alt="Тут эвакуатор"/>
+            </Link>
+            <div className={styles.rowBtn}>
+                <ul>
+                    <li><a href="#">о проекте</a></li>
+                    <li><a href="#">контакты</a></li>
+                </ul>
+                <Link to={'/create-ad'} className={clsx('btn', styles.btn)}>Добавить объявление</Link>
+            </div>
+        </div>
+    </header>;
 };
